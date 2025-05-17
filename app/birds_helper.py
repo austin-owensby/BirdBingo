7e64d979e85aad428cacefeb1010645ef23e9dd0
Austin Owensby <austin_owensby@hotmail.com>
2025-05-17 16:22:36 -0500

    Add remaining birds

--- app/birds_helper.py
+++ app/birds_helper.py
@@ -13,4 +13,184 @@ birds = [
     {
         "name": "Bald Eagle"
     },
+    {
+        "name": "Barn Owl"
+    },
+    {
+        "name": "Blue Coua"
+    },
+    {
+        "name": "Blue-Footed Booby"
+    },
+    {
+        "name": "Blue Jay"
+    },
+    {
+        "name": "Blue Tit"
+    },
+    {
+        "name": "Blue-Winged Pitta"
+    },
+    {
+        "name": "Bohemian Waxwing"
+    },
+    {
+        "name": "Brown Kiwi"
+    },
+    {
+        "name": "Brown Pelican"
+    },
+    {
+        "name": "Bullfinch"
+    },
+    {
+        "name": "California Condor"
+    },
+    {
+        "name": "Canada Goose"
+    },
+    {
+        "name": "Common Cuckoo"
+    },
+    {
+        "name": "Common Kingfisher"
+    },
+    {
+        "name": "Emperor Penguin"
+    },
+    {
+        "name": "Emu"
+    },
+    {
+        "name": "Eurasian Blackbird"
+    },
+    {
+        "name": "European Bee-Eater"
+    },
+    {
+        "name": "European Goldfinch"
+    },
+    {
+        "name": "European Robin"
+    },
+    {
+        "name": "European Starling"
+    },
+    {
+        "name": "Golden Oriole"
+    },
+    {
+        "name": "Golden Pheasant"
+    },
+    {
+        "name": "Gouldian Finch"
+    },
+    {
+        "name": "Great Buzzard"
+    },
+    {
+        "name": "Greater Flamingo"
+    },
+    {
+        "name": "Grey Heron"
+    },
+    {
+        "name": "Herring Gull"
+    },
+    {
+        "name": "Hoopoe"
+    },
+    {
+        "name": "House Martin"
+    },
+    {
+        "name": "House Sparrow"
+    },
+    {
+        "name": "Laughing Kookaburra"
+    },
+    {
+        "name": "Lilac-Breasted Roller"
+    },
+    {
+        "name": "Magnificent Bird-of-Paradise"
+    },
+    {
+        "name": "Magnificent Frigatebird"
+    },
+    {
+        "name": "Magpie"
+    },
+    {
+        "name": "Mallard"
+    },
+    {
+        "name": "Mandarin Duck"
+    },
+    {
+        "name": "Mute Swan"
+    },
+    {
+        "name": "Northern Cardinal"
+    },
+    {
+        "name": "Northern Mockingbird"
+    },
+    {
+        "name": "Ostrich"
+    },
+    {
+        "name": "Pied Wagtail"
+    },
+    {
+        "name": "Red-Billed Oxpecker"
+    },
+    {
+        "name": "Red Grouse"
+    },
+    {
+        "name": "Red-Headed Woodpecker"
+    },
+    {
+        "name": "Red Junglefowl"
+    },
+    {
+        "name": "Rhinoceros Hornbill"
+    },
+    {
+        "name": "Ruby-Throated Hummingbird"
+    },
+    {
+        "name": "Saddle-Billed Stork"
+    },
+    {
+        "name": "Scarlet Ibis"
+    },
+    {
+        "name": "Scarlet Macaw"
+    },
+    {
+        "name": "Shoebill"
+    },
+    {
+        "name": "Splendid Fairywren"
+    },
+    {
+        "name": "Superb Lyrebird"
+    },
+    {
+        "name": "Toco Toucan"
+    },
+    {
+        "name": "Turtle Dove"
+    },
+    {
+        "name": "Vulturine Guineafowl"
+    },
+    {
+        "name": "Wandering Albatross"
+    },
+    {
+        "name": "Wild Turkey"
+    },
 ]
