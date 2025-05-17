1bc9730b8b192e0a481d25c2769102259bc0e5b4
Austin Owensby <austin_owensby@hotmail.com>
2025-05-17 15:28:52 -0500

    Fetch draw history and start new game

--- app/requests.py
+++ app/requests.py
@@ -1,3 +1,5 @@
+import http
+from flask import Response
 from sqlalchemy import select
 from app.database.database_models import DrawHistory, get_session
 from . import app
@@ -7,6 +9,8 @@ import random
 @app.route("/api/draw-bird", methods=["POST"])
 def draw_card():
     """Based on drawn birds, randomly draw a new card then add that to the draw history"""
+
+    # TODO check for win condition before drawing
 
     # Query previously drawn birds from the database
     session = get_session()
@@ -19,7 +23,7 @@ def draw_card():
 
     # Check if there are no remaining birds (Shouldn't be possible unless we hit a race condition or someone is hitting the backend directly)
     if len(remaining_birds) == 0:
-        return 'No remaining birds are available to draw, please start a new game.', 400
+        return 'No remaining birds are available to draw, please start a new game.', http.HTTPStatus.BAD_REQUEST
 
     # Randomly choose a bird from our options
     drawn_bird: str = random.choice(list(remaining_birds))
@@ -34,3 +38,35 @@ def draw_card():
 
     # Return the drawn bird to the frontend
     return drawn_bird
+
+@app.route("/api/draw-history", methods=["GET"])
+def get_draw_history():
+    """Return the list of draw history"""
+
+    # Query all draw history
+    session = get_session()
+    query = select(DrawHistory.name)
+
+    draw_history = session.scalars(query).all()
+
+    return draw_history
+
+@app.route("/api/new-game", methods=["POST"])
+def new_game():
+    """Start a new bingo game"""
+    # TODO check if game has finished before starting a new one
+    # TODO generate new boards
+
+    # Query all draw history
+    session = get_session()
+    query = select(DrawHistory)
+
+    draw_history = session.scalars(query).all()
+
+    # Delete and save all draw history
+    for draw_history_item in draw_history:
+        session.delete(draw_history_item)
+
+    session.commit()
+    
+    return Response(status=http.HTTPStatus.NO_CONTENT)
--- app/static/site.js
+++ app/static/site.js
@@ -1,6 +1,8 @@
 let drawHistory = [];
 
 function drawBird() {
+    toggleLoadingButtons(true);
+
     fetch('/api/draw-bird', { method: 'POST'})
         .then(response => {
             if (!response.ok) {
@@ -11,26 +13,107 @@ function drawBird() {
             return response.text();
         })
         .then(newDrawHistory => {
-            const drawHistoryList = document.getElementById('draw-history');
-
             drawHistory.push(newDrawHistory);
-
-            let listHTML = '';
-
-            for (let drawHistoryItem of drawHistory) {
-                listHTML += `<li>${drawHistoryItem}</li>`;
-            }
-
-            drawHistoryList.innerHTML = listHTML;
         })
         .catch(error => {
             const errorMessage = document.getElementById('error-message');
             errorMessage.innerHTML = error;
+        })
+        .finally(() => {
+            toggleLoadingButtons(false);
+            toggleListLoading(false);
+        });
+}
+
+function fetchDrawHistory() {
+    toggleListLoading(true);
+    toggleLoadingButtons(true);
+
+    fetch('/api/draw-history', { method: 'GET'})
+        .then(response => {
+            if (!response.ok) {
+                return response.text().then(error_message => {
+                    throw new Error(error_message ?? response.statusText);
+                })
+            }
+            return response.json();
+        })
+        .then(allDrawHistory => {
+            drawHistory = allDrawHistory;
+        })
+        .catch(error => {
+            const errorMessage = document.getElementById('error-message');
+            errorMessage.innerHTML = error;
+        })
+        .finally(() => {
+            toggleLoadingButtons(false);
+            toggleListLoading(false);
+        });
+}
+
+function startNewGame() {
+    toggleListLoading(true);
+    toggleLoadingButtons(true);
+
+    fetch('/api/new-game', { method: 'POST'})
+        .then(response => {
+            if (!response.ok) {
+                return response.text().then(error_message => {
+                    throw new Error(error_message ?? response.statusText);
+                })
+            }
+            return;
+        })
+        .then(() => {
+            drawHistory = [];
+        })
+        .catch(error => {
+            const errorMessage = document.getElementById('error-message');
+            errorMessage.innerHTML = error;
+        })
+        .finally(() => {
+            toggleLoadingButtons(false);
+            toggleListLoading(false);
+        });
+}
+
+function toggleLoadingButtons(loading) {
+    const drawBirdButton = document.getElementById('draw-bird');
+    drawBirdButton.disabled = loading;
+
+    const newGameButton = document.getElementById('new-game');
+    newGameButton.disabled = loading;
+
+    // Clear the error, potentially move this to a toast message that's timed
+    if (loading) {
+        const errorMessage = document.getElementById('error-message');
+        errorMessage.innerHTML = '';
+    }
+}
+
+function toggleListLoading(loading) {
+    const drawHistoryList = document.getElementById('draw-history');
+
+    if (loading) {
+        drawHistoryList.innerHTML = 'Loading...';
+    }
+    else {
+        let listHTML = '';
+
+        for (let drawHistoryItem of drawHistory) {
+            listHTML += `<li>${drawHistoryItem}</li>`;
         }
-    );
+
+        drawHistoryList.innerHTML = listHTML;
+    }
 }
 
 window.onload = function() {
     const drawBirdButton = document.getElementById('draw-bird');
     drawBirdButton.addEventListener('click', drawBird);
+
+    const newGameButton = document.getElementById('new-game');
+    newGameButton.addEventListener('click', startNewGame);
+
+    fetchDrawHistory();
 }
